b11ab3c187573cf3453a53b3b638aa5b270e78c8
Vincent <vince@iwit-systems.fr>
2026-05-12 14:03:30 +0200

    feat: some title bar work

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,10 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  i18n: {
+    locales: ['en', 'de', 'fr'],
+    defaultLocale: 'en',
+  },
   vite: {
     plugins: [tailwindcss()],
   },
